ac932e3b6b79065da3619588a69841c2f2e74bbc
tolegenJob <tolegen.job@gmail.com>
2025-06-07 02:07:55 +0500

    fix: validation fixed

--- src/main/java/com/example/stoservice/dto/request/LoginRequest.java
+++ src/main/java/com/example/stoservice/dto/request/LoginRequest.java
@@ -8,7 +8,7 @@ public record LoginRequest(
 
         @NotBlank(message = "Email must not be blank")
         @Email(message = "Email should be valid")
-        @Size(min = 4, max = 20, message = "Username must be between 4 and 20 symbols")
+        @Size(min = 4, max = 20, message = "Email must be between 4 and 20 symbols")
         String email,
 
         @NotBlank(message = "Password must not be blank")
--- src/main/java/com/example/stoservice/dto/request/RegisterRequest.java
+++ src/main/java/com/example/stoservice/dto/request/RegisterRequest.java
@@ -12,12 +12,12 @@ public record RegisterRequest(
         String firstName,
 
         @NotBlank(message = "Last name must not be blank")
-        @Email(message = "Email should be valid")
         @Size(min = 2, max = 30, message = "Last name must be between 2 and 30 characters")
         String lastName,
 
         @NotBlank(message = "Email must not be blank")
-        @Size(min = 4, max = 20, message = "Username must be between 4 and 20 symbols")
+        @Email(message = "Email should be valid")
+        @Size(min = 4, max = 20, message = "Email must be between 4 and 20 symbols")
         String email,
 
         @NotBlank(message = "Password must not be blank")
@@ -30,7 +30,7 @@ public record RegisterRequest(
         @NotBlank(message = "Phone number must not be blank")
         @Pattern(
                 regexp = "^\\+7(?:\\s*\\d){10}$",
-                message = "Phone number must start with +7 и содержать ровно 10 цифр, между которыми допускаются любые пробелы"
+                message = "Phone number must start with +7 and consists of 10 digits"
         )
         String phoneNumber
 
